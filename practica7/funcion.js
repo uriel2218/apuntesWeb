7f53f64171d04f36b7029b6e3c79a810fee8a1cb
uriel2218 <uriel22_18@protonmail.com>
2026-04-14 10:53:40 -0600

    Se acualiza práctica 7 de JS

--- practica7/funcion.js
+++ practica7/funcion.js
@@ -12,30 +12,49 @@ let citas = [];
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    const mascota = document.getElementById("nombreMascota").value;
-    const tipo = document.getElementById("tipoMascota").value;
-    const fecha = document.getElementById("fecha").value;
-    const dueno = document.getElementById("nombrePersona").value;
-    const telefono = document.getElementById("telefonoPersona").value;
 
-    if(!mascota || !fecha || !telefono){
+    const datos = leerDatos();
+
+    validarDatos(datos.mascota, datos.tipo, datos.fecha);
+
+    guardarCita(datos);
+
+    
+    mostrarCitas();
+})
+
+function leerDatos(){
+    return {
+        mascota: document.getElementById("nombreMascota").value,
+        tipo: document.getElementById("tipoMascota").value,
+        fecha: document.getElementById("fecha").value,
+        dueno: document.getElementById("nombrePersona").value,
+        telefono: document.getElementById("telefonoPersona").value,
+        creado: Date.now()
+
+    }
+
+}
+
+function validarDatos(mascota,tipo,fecha){
+    if(mascota == "" || tipo == ""  || fecha == "" ){
         alert("Todos los campos son necesarios");
         return;
     }
+}
 
+function guardarCita(datos){
     const nuevaCita = {
-        mascota,
-        tipo,
-        fecha,
-        dueno,
-        telefono,
+        mascota: datos.mascota,
+        tipo: datos.tipo,
+        fecha: datos.fecha,
+        dueno: datos.dueno,
+        telefono: datos.telefono,
         creado: Date.now()
     }
 
     citas.push(nuevaCita);
-    console.log(citas);
-    mostrarCitas();
-})
+}
 
 function mostrarCitas(){
     list.innerHTML = "";
@@ -52,10 +71,14 @@ function mostrarCitas(){
         <br>
         <strong>Teléfono: </strong> ${cita.telefono}
         <br>
+        <button onclick="eliminar(${cita.creado})">Eliminar </button>
         `
         
         list.appendChild(li);
     });
 }
 
-
+function eliminar(id){
+    citas = citas.filter(cita => cita.creado !== id)
+    mostrarCitas();
+}
